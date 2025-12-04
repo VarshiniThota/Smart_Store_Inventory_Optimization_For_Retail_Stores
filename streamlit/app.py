fe9dad654240c366b49d5a0929a9e3a9aa66f14b
Varshini Thota <varshinipatel001@gmail.com>
2025-12-04 20:10:22 +0530

    Add top 5 best selling products visualization

--- streamlit/app.py
+++ streamlit/app.py
@@ -7,25 +7,25 @@ st.title("SmartStore Inventory Optimization Dashboard")
 file_path="../original_files/updated_dataset.csv"
 df=pd.read_csv(file_path)
 
-st.subheader("Total Sales by Product")
-fig1 = px.bar(df, x="Product_Name", y="Total_Sales_Value",
-              color="Total_Sales_Value")
+st.subheader("Top 5 Best Selling Products")
+category_sales = df.groupby("Product_Name")["Total_Sales_Value"].sum().reset_index()
+top_products = category_sales.sort_values(by="Total_Sales_Value", ascending=False).head(5)
+fig1 = px.bar(top_products, x="Product_Name", y="Total_Sales_Value",color="Total_Sales_Value", title="Top 5 Products")
 st.plotly_chart(fig1)
 
 st.subheader("Sales Trend by Products")
 category_sales = df.groupby("Product_Name")["Total_Sales_Value"].sum().reset_index()
-fig2 = px.line(category_sales, x="Product_Name", y="Total_Sales_Value", markers=True,
-              title="Sales Trend by products")
+fig2 = px.line(category_sales, x="Product_Name", y="Total_Sales_Value", markers=True,title="Sales Trend by products")
 st.plotly_chart(fig2)
 
 
 
 st.subheader("Price vs Units Sold")
-fig3 = px.scatter(df, x="Price", y="Units_Sold",
-                  size="Opening_Stock", color="Product_Name")
+fig3 = px.scatter(df, x="Price", y="Units_Sold",size="Opening_Stock", color="Product_Name")
 st.plotly_chart(fig3)
 
 st.subheader("Stock-out Risk Products")
 df["Sales_to_Stock_Ratio"] = df["Units_Sold"] / df["Opening_Stock"]
 df["Stockout_Risk"] = df["Sales_to_Stock_Ratio"] > 0.7
 st.dataframe(df[df["Stockout_Risk"] == True])
+
